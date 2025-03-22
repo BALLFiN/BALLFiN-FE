6e842b5095b71b264c74c4ffbb9c963b09bc830a
천성윤 <1112csy@naver.com>
2025-03-22 18:02:20 +0900

    🔧: 영어글꼴 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Noto Sans KR"', "sans-serif"],
+        shrikhand: ["Shrikhand", "serif"],
+      },
       keyframes: {
         fadeIn: {
           "0%": { opacity: "0" },
@@ -19,25 +23,25 @@ export default {
           },
         },
         slideRightFade: {
-          "0%": {
-            transform: "translateX(30px) scale(0.95)",
-            opacity: "0",
-          },
-          "100%": {
-            transform: "translateX(0) scale(1)",
-            opacity: "1",
-          },
+          "0%": { opacity: "0", transform: "translateX(-10px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+        slideUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        pageEnter: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
       },
       animation: {
-        fadeIn: "fadeIn 0.3s ease-out forwards",
+        fadeIn: "fadeIn 0.5s ease-out",
         slideLeftFade:
           "slideLeftFade 0.4s cubic-bezier(0.16, 1, 0.3, 1) forwards",
-        slideRightFade:
-          "slideRightFade 0.4s cubic-bezier(0.16, 1, 0.3, 1) forwards",
-      },
-      fontFamily: {
-        sans: ['"Noto Sans KR"', "sans-serif"],
+        slideRightFade: "slideRightFade 0.5s ease-out",
+        slideUp: "slideUp 0.5s ease-out",
+        pageEnter: "pageEnter 0.5s ease-out",
       },
     },
   },
