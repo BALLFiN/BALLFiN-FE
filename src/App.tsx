1fd49079db311eda54026d93511047272819d258
keeprok <140785214+keeprok@users.noreply.github.com>
2025-05-06 23:47:05 +0900

    🔧 : query 설정
    query : 설치
    query provider : 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/layout/Header";
-import HomePage from "./pages/HomePage";
-import IntroPage from "./pages/introPage";
-import NewsPage from "@/pages/news/newsPage";
-import StockPage from "@/pages/stock/stockPage";
-import LoginPage from "@/pages/auth/loginPage";
-import SignUpPage from "@/pages/auth/signUpPage";
-import MyPage from "@/pages/auth/myPage";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import HomePage from './pages/HomePage';
+import IntroPage from './pages/introPage';
+import NewsPage from '@/pages/news/newsPage';
+import StockPage from '@/pages/stock/stockPage';
+import LoginPage from '@/pages/auth/loginPage';
+import SignUpPage from '@/pages/auth/signUpPage';
+import MyPage from '@/pages/auth/myPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Header />
-        <main className="pt-16">
-          <Routes>
-            <Route path="/intro" element={<IntroPage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/stock" element={<StockPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/mypage" element={<MyPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="min-h-screen bg-white">
+          <Header />
+          <main className="pt-16">
+            <Routes>
+              <Route path="/intro" element={<IntroPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/news" element={<NewsPage />} />
+              <Route path="/stock" element={<StockPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/mypage" element={<MyPage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
