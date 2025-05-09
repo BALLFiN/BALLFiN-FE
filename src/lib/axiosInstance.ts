992a5ed8af296839dd6d3ae52b849259eefdcfd2
keeprok <140785214+keeprok@users.noreply.github.com>
2025-05-09 20:40:02 +0900

    ⚡️ : axiosInstance 수정

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://44.207.10.254', // vite config 수정하고 변경예정
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use((config) => {
