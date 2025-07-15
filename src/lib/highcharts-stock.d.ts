2bd5e9d759dd9cc54f6c3a86caf2982734954001
keeprok <140785214+keeprok@users.noreply.github.com>
2025-07-15 23:50:46 +0900

    🔧 : 기능전 정보수정

--- src/lib/highcharts-stock.d.ts
+++ src/lib/highcharts-stock.d.ts
@@ -3,3 +3,9 @@ declare module 'highcharts/modules/stock' {
   function StockModule(H: typeof Highcharts): void;
   export = StockModule;
 }
+declare module 'highcharts/modules/annotations' {
+  import * as Highcharts from 'highcharts';
+
+  function AnnotationsModule(H: typeof Highcharts): void;
+  export = AnnotationsModule;
+}
