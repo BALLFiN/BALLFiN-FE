dbbff7d8c79b86d6d084153874a70fd1de824d95
keeprok <140785214+keeprok@users.noreply.github.com>
2025-08-05 23:56:34 +0900

    🔖 : myPage -companies data

---
+++ src/components/auth/myPage/mock/companies.ts
@@ -0,0 +1,394 @@
+export const myPageCompanies = [
+  {
+    corp_name: '삼성바이오로직스',
+    stock_code: '207940',
+  },
+  {
+    corp_name: '기아',
+    stock_code: '000270',
+  },
+  {
+    corp_name: '셀트리온',
+    stock_code: '068270',
+  },
+  {
+    corp_name: 'NAVER',
+    stock_code: '035420',
+  },
+  {
+    corp_name: '메리츠금융지주',
+    stock_code: '138040',
+  },
+  {
+    corp_name: 'HMM',
+    stock_code: '011200',
+  },
+  {
+    corp_name: 'LG화학',
+    stock_code: '051910',
+  },
+  {
+    corp_name: '삼성생명',
+    stock_code: '032830',
+  },
+  {
+    corp_name: '고려아연',
+    stock_code: '010130',
+  },
+  {
+    corp_name: 'KT',
+    stock_code: '030200',
+  },
+  {
+    corp_name: 'HD현대일렉트릭',
+    stock_code: '267260',
+  },
+  {
+    corp_name: 'LG전자',
+    stock_code: '066570',
+  },
+  {
+    corp_name: '포스코인터내셔널',
+    stock_code: '047050',
+  },
+  {
+    corp_name: '한국항공우주',
+    stock_code: '047810',
+  },
+  {
+    corp_name: '한화시스템',
+    stock_code: '272210',
+  },
+  {
+    corp_name: 'LIG넥스원',
+    stock_code: '079550',
+  },
+  {
+    corp_name: '미래에셋증권',
+    stock_code: '006800',
+  },
+  {
+    corp_name: '코웨이',
+    stock_code: '021240',
+  },
+  {
+    corp_name: 'LG유플러스',
+    stock_code: '032640',
+  },
+  {
+    corp_name: '한국금융지주',
+    stock_code: '071050',
+  },
+  {
+    corp_name: '삼성E&A',
+    stock_code: '028050',
+  },
+  {
+    corp_name: 'CJ',
+    stock_code: '001040',
+  },
+  {
+    corp_name: '레인보우로보틱스',
+    stock_code: '277810',
+  },
+  {
+    corp_name: '삼성전자',
+    stock_code: '005930',
+  },
+  {
+    corp_name: 'LG에너지솔루션',
+    stock_code: '373220',
+  },
+  {
+    corp_name: '현대자동차',
+    stock_code: '005380',
+  },
+  {
+    corp_name: '삼성물산',
+    stock_code: '028260',
+  },
+  {
+    corp_name: '하나금융지주',
+    stock_code: '086790',
+  },
+  {
+    corp_name: '우리금융지주',
+    stock_code: '316140',
+  },
+  {
+    corp_name: '기업은행',
+    stock_code: '024110',
+  },
+  {
+    corp_name: '현대로템',
+    stock_code: '064350',
+  },
+  {
+    corp_name: '포스코퓨처엠',
+    stock_code: '003670',
+  },
+  {
+    corp_name: 'SK',
+    stock_code: '034730',
+  },
+  {
+    corp_name: '삼성에스디에스',
+    stock_code: '018260',
+  },
+  {
+    corp_name: 'SK바이오팜',
+    stock_code: '326030',
+  },
+  {
+    corp_name: 'HD현대마린솔루션',
+    stock_code: '443060',
+  },
+  {
+    corp_name: 'HD현대',
+    stock_code: '267250',
+  },
+  {
+    corp_name: 'LS ELECTRIC',
+    stock_code: '010120',
+  },
+  {
+    corp_name: 'LG생활건강',
+    stock_code: '051900',
+  },
+  {
+    corp_name: 'LG씨엔에스',
+    stock_code: '064400',
+  },
+  {
+    corp_name: '한국타이어앤테크놀로지',
+    stock_code: '161390',
+  },
+  {
+    corp_name: '오리온',
+    stock_code: '271560',
+  },
+  {
+    corp_name: '삼성카드',
+    stock_code: '029780',
+  },
+  {
+    corp_name: '현대건설',
+    stock_code: '000720',
+  },
+  {
+    corp_name: 'NH투자증권',
+    stock_code: '005940',
+  },
+  {
+    corp_name: '효성중공업',
+    stock_code: '298040',
+  },
+  {
+    corp_name: 'LG디스플레이',
+    stock_code: '034220',
+  },
+  {
+    corp_name: '에코프로머티',
+    stock_code: '450080',
+  },
+  {
+    corp_name: '카카오페이',
+    stock_code: '377300',
+  },
+  {
+    corp_name: '한화솔루션',
+    stock_code: '009830',
+  },
+  {
+    corp_name: 'LS',
+    stock_code: '006260',
+  },
+  {
+    corp_name: '넷마블',
+    stock_code: '251270',
+  },
+  {
+    corp_name: '에코프로비엠',
+    stock_code: '247540',
+  },
+  {
+    corp_name: '에코프로',
+    stock_code: '086520',
+  },
+  {
+    corp_name: '한화에어로스페이스',
+    stock_code: '012450',
+  },
+  {
+    corp_name: 'KB금융',
+    stock_code: '105560',
+  },
+  {
+    corp_name: 'HD현대중공업',
+    stock_code: '329180',
+  },
+  {
+    corp_name: '신한지주',
+    stock_code: '055550',
+  },
+  {
+    corp_name: '현대모비스',
+    stock_code: '012330',
+  },
+  {
+    corp_name: '한화오션',
+    stock_code: '042660',
+  },
+  {
+    corp_name: 'POSCO홀딩스',
+    stock_code: '005490',
+  },
+  {
+    corp_name: '삼성화재',
+    stock_code: '000810',
+  },
+  {
+    corp_name: 'SK이노베이션',
+    stock_code: '096770',
+  },
+  {
+    corp_name: '한국전력',
+    stock_code: '015760',
+  },
+  {
+    corp_name: '두산에너빌리티',
+    stock_code: '034020',
+  },
+  {
+    corp_name: '삼성중공업',
+    stock_code: '010140',
+  },
+  {
+    corp_name: '삼성SDI',
+    stock_code: '006400',
+  },
+  {
+    corp_name: 'SK텔레콤',
+    stock_code: '017670',
+  },
+  {
+    corp_name: '하이브',
+    stock_code: '352820',
+  },
+  {
+    corp_name: '유한양행',
+    stock_code: '000100',
+  },
+  {
+    corp_name: '아모레퍼시픽',
+    stock_code: '090430',
+  },
+  {
+    corp_name: 'S-Oil',
+    stock_code: '010950',
+  },
+  {
+    corp_name: '한진칼',
+    stock_code: '180640',
+  },
+  {
+    corp_name: '맥쿼리인프라',
+    stock_code: '088980',
+  },
+  {
+    corp_name: 'HD현대미포',
+    stock_code: '010620',
+  },
+  {
+    corp_name: '두산',
+    stock_code: '000150',
+  },
+  {
+    corp_name: '두산밥캣',
+    stock_code: '241560',
+  },
+  {
+    corp_name: '삼성증권',
+    stock_code: '016360',
+  },
+  {
+    corp_name: 'SKC',
+    stock_code: '011790',
+  },
+  {
+    corp_name: 'CJ제일제당',
+    stock_code: '097950',
+  },
+  {
+    corp_name: '알테오젠',
+    stock_code: '196170',
+  },
+  {
+    corp_name: 'HLB',
+    stock_code: '028300',
+  },
+  {
+    corp_name: 'SK하이닉스',
+    stock_code: '000660',
+  },
+  {
+    corp_name: '크래프톤',
+    stock_code: '259960',
+  },
+  {
+    corp_name: '카카오',
+    stock_code: '035720',
+  },
+  {
+    corp_name: 'HD한국조선해양',
+    stock_code: '009540',
+  },
+  {
+    corp_name: 'KT&G',
+    stock_code: '033780',
+  },
+  {
+    corp_name: 'SK스퀘어',
+    stock_code: '402340',
+  },
+  {
+    corp_name: '카카오뱅크',
+    stock_code: '323410',
+  },
+  {
+    corp_name: 'LG',
+    stock_code: '003550',
+  },
+  {
+    corp_name: '삼성전기',
+    stock_code: '009150',
+  },
+  {
+    corp_name: '현대글로비스',
+    stock_code: '086280',
+  },
+  {
+    corp_name: '대한항공',
+    stock_code: '003490',
+  },
+  {
+    corp_name: '삼양식품',
+    stock_code: '003230',
+  },
+  {
+    corp_name: '한미반도체',
+    stock_code: '042700',
+  },
+  {
+    corp_name: 'DB손해보험',
+    stock_code: '005830',
+  },
+  {
+    corp_name: '포스코DX',
+    stock_code: '022100',
+  },
+  {
+    corp_name: 'GS',
+    stock_code: '078930',
+  },
+];
