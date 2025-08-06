f4e6d975c768cf68590eee3363523f9c2132586b
keeprok <140785214+keeprok@users.noreply.github.com>
2025-08-06 23:55:30 +0900

    🎨 : main-ProfileCard 생성

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -1,3 +1,13 @@
+import ProfileCard from '@/components/auth/myPage/main/ProfileCard';
+
 export default function MyPage() {
-  return <div className="min-h-screen bg-gray-50"></div>;
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      <ProfileCard /> {/* 전체 너비 */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {/* <FavoriteCharts />
+        <FavoriteNews /> */}
+      </div>
+    </div>
+  );
 }
